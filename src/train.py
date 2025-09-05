3f6724a826a35016e578d7fbe621385aba3eb867
Maru <mariaeu.alvarez@gmail.com>
2025-09-05 14:28:59 +0200

    chore(chore/implement-train): implement training code

---
+++ src/train.py
@@ -0,0 +1,28 @@
+import numpy as np
+from modeling import model 
+from tqdm import tqdm
+from data_process import *
+
+# Define size and number of batches
+bs = 32
+num_batches = len(X_train) // bs
+
+# Instantiate objects from classes 'CrossEntropyLoss' and 'Optim.Adam'
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+# Training de model:
+for epoch in range(10):
+    for b in tqdm(range(num_batches)):
+        x = X_train[b * bs : (b + 1) * bs]
+        y = y_train[b * bs : (b + 1) * bs]
+        y_hat = model(x)  # prediction from the model
+
+        # Calculate loss function with labels and predicitions as parameters
+        loss = loss_fn(y_hat, y)
+
+        # Gradients represent the magnitude and direction in which the weights should be adjusted to optimize the model.
+        optimizer.zero_grad()  # Gradient reset to 0
+        loss.backward()  # Calculate how to adjust model's weights to improve predictions
+        optimizer.step()  # Apply calculate changes
+    print(f"Epoch {epoch+1} loss: {loss.item():.3f}")
